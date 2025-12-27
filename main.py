246f7d4177ceb8b473a13bb177c4f8ec15b59777
Amala Pasham <amalapasham@example.com>
2025-12-27 15:14:48 +0000

    Clean up nested folder structure

--- main.py
+++ main.py
@@ -110,4 +110,4 @@ def delete_student(student_id: int):
 # Server run
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8003)
